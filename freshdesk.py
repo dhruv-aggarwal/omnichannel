86add659b81584302f66856d5a3327be51fe0b3f
Dhruv Aggarwal <dhruvaggarwal043@gmail.com>
2018-05-11 02:09:16 +0530

    Add freshdesk script

---
+++ freshdesk.py
@@ -0,0 +1,75 @@
+import requests
+from settings import CRM
+
+
+class Freshdesk:
+    def __init__(self):
+        self.domain = CRM['freshdesk']['domain']
+        self.api_key = CRM['freshdesk']['api_key']
+        self.password = CRM['freshdesk']['password']
+        self.base_url = 'https://{domain}.freshdesk.com/api/v2/'.format(
+            domain=self.domain
+        )
+        self.fetch_url = 'tickets'
+        self.page_size = 100
+        self.page_limit = 10
+        self.source_map = {
+            '1': 'Email',
+            '2': 'Portal',
+            '3': 'Phone',
+            '7': 'Chat',
+            '8': 'Mobihelp',
+            '9': 'Feedback Widget',
+            '10': 'Outbound Email'
+        }
+        self.status_map = {
+            '2': 'open',
+            '3': 'pending',
+            '4': 'resolved',
+            '5': 'closed'
+        }
+        self.priority_map = {
+            '1': 'low',
+            '2': 'medium',
+            '3': 'high',
+            '4': 'urgent'
+        }
+
+    def fetch_tickets(self):
+        page = 0
+        responses = []
+        while page < self.page_limit:
+            page += 1
+            response = requests.get(
+                self.base_url + self.fetch_url,
+                params={
+                    'order_by': 'created_at',
+                    'order_type': 'desc',
+                    'per_page': self.page_size,
+                    'page': page
+                },
+                auth=(self.api_key, self.password)
+            )
+            responses += response.json()
+        return self.parse_response(responses)
+
+    def parse_response(self, responses):
+        parsed = []
+        for item in responses:
+            parsed.append({
+                'subject': item['subject'],
+                'created_at': item['created_at'],
+                'description_text': item['description_text'],
+                'product_id': item['product_id'],
+                'email': item.get('email'),
+                'sentiment': item['custom_fields'].get('sentiment'),
+                'csat_rating': item['custom_fields'].get('csat_rating'),
+                'country': item['custom_fields'].get('country'),
+                'platform': item['custom_fields'].get('platform'),
+                'practo_source': item['custom_fields'].get('practo_source'),
+                'practo_product': item['custom_fields'].get('practo_product'),
+                'source': self.source_map.get(item['source']),
+                'priority': self.priority_map.get(item['priority']),
+                'type': item['type']
+            })
+        return parsed
